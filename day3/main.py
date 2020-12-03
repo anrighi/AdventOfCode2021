16236979d1b8fafc9a9424e9ab57b64ceef59f40
Andrea Righi <an.righi7@gmail.com>
2020-12-03 08:49:42 +0100

    completed day3

--- day3/main.py
+++ day3/main.py
@@ -1,64 +1,47 @@
 import os
+import time
 
 from utility.file_reader import read_file
 from utility.parser import convert_to_int
 
 
-def part_1(value_list):
+def path_traversal(value_list, right_step, down_step):
+    path_idx = 0
     counter = 0
 
+    path_length = len(value_list[0])
+
     for idx, value in enumerate(value_list):
-        if idx % 3 == 0:
-            value_range = convert_to_int(value.split('-'))
 
-            letter = value_list[idx + 1][0:1]
-            password = value_list[idx + 2]
+        if idx % down_step == 0:
+            single_path = value
+            tree_or_space = single_path[path_idx]
 
-            letter_occurrences = password.count(letter)
-
-            if value_range[0] <= letter_occurrences <= value_range[1]:
+            if tree_or_space == '#':
                 counter += 1
+
+            path_idx += right_step
+
+            if path_idx >= path_length:
+                path_idx = path_idx % path_length
 
     return counter
 
 
-# RecursionError: maximum recursion depth exceeded while calling a Python object
-def part_1_recursive(value_list, counter):
-    print(len(value_list))
+def part_1(value_list):
+    right_3_down_1 = path_traversal(value_list, 3, 1)
 
-    if len(value_list) == 0:
-        return counter
-    else:
-        value_range = convert_to_int(value_list[0].split('-'))
-        letter = value_list[1][0:1]
-        password = value_list[2]
-
-        letter_occurrences = password.count(letter)
-
-        if value_range[0] <= letter_occurrences <= value_range[1]:
-            counter += 1
-
-        return part_1_recursive(value_list[3:], counter)
+    return right_3_down_1
 
 
 def part_2(value_list):
-    counter = 0
+    right_1_down_1 = path_traversal(value_list, 1, 1)
+    right_3_down_1 = path_traversal(value_list, 3, 1)
+    right_5_down_1 = path_traversal(value_list, 5, 1)
+    right_7_down_1 = path_traversal(value_list, 7, 1)
+    right_1_down_2 = path_traversal(value_list, 1, 2)
 
-    for idx, value in enumerate(value_list):
-        if idx % 3 == 0:
-            value_range = convert_to_int(value.split('-'))
-
-            letter = value_list[idx + 1][0:1]
-            password = value_list[idx + 2]
-
-            idx0 = value_range[0] - 1
-            idx1 = value_range[1] - 1
-
-            if password[idx0] == letter or password[idx1] == letter:
-                if password[idx0] != password[idx1]:
-                    counter += 1
-
-    return counter
+    return right_1_down_1 * right_1_down_2 * right_3_down_1 * right_5_down_1 * right_7_down_1
 
 
 if __name__ == '__main__':
@@ -66,5 +49,10 @@ if __name__ == '__main__':
 
     input_read = read_file(dir_path + "/" + "input1.txt")
 
+    start_time = time.time()
     print('part 1:', part_1(input_read))
+    print("--- %s seconds ---" % (time.time() - start_time), "\n")
+
+    start_time = time.time()
     print('part 2:', part_2(input_read))
+    print("--- %s seconds ---" % (time.time() - start_time), "\n")
